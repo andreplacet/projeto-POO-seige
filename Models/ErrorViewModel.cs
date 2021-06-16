aa2c4b835bb5178a79ec037d2e0b30913d7f46a5
andre <andreplacet@gmail.com>
2021-06-16 19:17:06 -0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace projeto_POO_seige.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
